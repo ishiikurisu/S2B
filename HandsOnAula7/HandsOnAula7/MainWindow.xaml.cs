c068a4d800a9421503e0e3897d7947e6a8e1ca77
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-11-18 08:57:25 -0200

    Maybe I should study data binding

---
+++ HandsOnAula7/HandsOnAula7/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace HandsOnAula7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Rectangle_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Window1 w1 = new Window1();
+            w1.Show();
+            this.Hide();
+        }
+
+        private void Rectangle_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Window2 w2 = new Window2();
+            w2.Show();
+            this.Hide();
+        }
+
+        private void GreenRect_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Window3 w3 = new Window3();
+            w3.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ HandsOnAula7/HandsOnAula7/Window1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace HandsOnAula7
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Rectangle_MouseEnter(object sender, MouseEventArgs e)
+        {
+            FriendBlock.Fill = new SolidColorBrush(Colors.Blue);
+        }
+
+        private void Rectangle_MouseLeave(object sender, MouseEventArgs e)
+        {
+            FriendBlock.Fill = new SolidColorBrush(Colors.Tomato);
+        }
+
+        private void BadBlock_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            this.Hide();
+            mw.Show();
+        }
+    }
+}
